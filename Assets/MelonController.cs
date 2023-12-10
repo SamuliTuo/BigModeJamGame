1524bc466658c46a0994862e1d1afcd6ab0aee36
smuli <smuli@studio>
2023-12-10 18:55:48 +0200

    melons are work

--- Assets/MelonController.cs
+++ Assets/MelonController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MelonController : MonoBehaviour
 {
+    public int value = 1;
+    [HideInInspector] public bool wasCollected = false;
+
     [SerializeField] private float rotateSpeed = 1.0f;
     [SerializeField] private float bobSpeed = 1.0f;
     [SerializeField] private float bobRange = 1.0f;
@@ -23,5 +26,11 @@ public class MelonController : MonoBehaviour
         float distance = Mathf.Sin(Time.timeSinceLevelLoad * bobSpeed);
         model.position = startPos + Vector3.up * distance * bobRange;
         model.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+
+    public void ThisWasCollected()
+    {
+        wasCollected = true;
+        Destroy(gameObject);
     }
 }
